51750c7547af1a91dc7e8ca3a5d433ca36141b70
Elspeth <elspeth@mardaljevic.org>
2024-08-12 16:46:01 +0100

    test editing

--- main.js
+++ main.js
@@ -9,7 +9,21 @@
 // Your function responses should mirror responses in the test cases, and your implementation should handle positive integers
 
 // Skeleton function
-export function calculateSprinkles(number) {
-    // Happy coding! 
-}
 
+module.exports = function calculateSprinkles (number) {
+    // checking if number is an integer, and more than zero
+    if (!Number.isInteger(number) || number <= 0) {
+        return "Invalid input: Please provide a positive integer";
+    }
+    // main function logic 
+    if (number % 7 === 0 && number % 3 === 0) {
+    return "Great choice - number is divisible by both 3 and 7"
+    } else if (number % 7 === 0 && number % 3 !== 0) {
+    return "Number is divisible by 7, but not 3" 
+    } else if (number % 7 !== 0 && number % 3 === 0) {
+   return "Number is divisible by 3, but not 7" 
+    } else if (number % 7 !== 0 && number % 3 !== 0) {
+    return "Worst choice, number is not divisible by either 7 or 3"
+   } else if (number === 0)
+   return "Number must be a positive integer"
+}
